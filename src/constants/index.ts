3d9b9539515fa33239b366d3b6076d31207cd975
InfernalHeir <bhupendrabisht61049@gmail.com>
2021-07-01 18:04:07 +0530

    using new contracts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,2 @@
-export const UFARMBeneficiaryBookAddress: string =
-   "0xfaf9329efc2b1c10c16e9f3c27622f696b41bf7a";
-
-export const beneficiaryServerEndpoint =
-   "https://dev.api.unifarm.co/beneficiaries";
+export const UFARMBeneficiaryBookAddress: string = "0xAe93546f649F451E1269f6388FE23F726FB141c7";
+export const beneficiaryServerEndpoint = "http://localhost:3001/beneficiaries";
