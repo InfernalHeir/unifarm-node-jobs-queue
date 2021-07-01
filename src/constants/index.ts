13349c0bf3dd76566dcb3d39111af391c8d1cead
InfernalHeir <bhupendrabisht61049@gmail.com>
2021-07-01 18:38:36 +0530

    changes in beneficiary bas url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export const UFARMBeneficiaryBookAddress: string = "0xAe93546f649F451E1269f6388FE23F726FB141c7";
-export const beneficiaryServerEndpoint = "http://localhost:3001/beneficiaries";
+export const beneficiaryServerEndpoint = "https://dev.api.unifarm.co/beneficiaries";
