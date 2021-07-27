e872fd50d82914a4b35e9c76e29f08177803d18e
Bhupendra Bisht <bhupendrabisht61049@gmail.com>
2021-07-27 11:06:31 +0530

    beneficiary erndpoint change

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const UFARMBeneficiaryBookAddress: string = "0xDdEB3541985Cb084A3bCC6Bfd85e69CFF3F1d39F";
-export const beneficiaryServerEndpoint = "http://localhost:8000/beneficiaries";
+export const beneficiaryServerEndpoint = "https://dev.api.unifarm.co/beneficiaries";
 
 export const ADMIN_WALLET:string = "0x0bfc689bfd3a457278afc7e72a72d80d6a968280"
 // 15 seconds per block 4*12.5 = 12.5 minuates
