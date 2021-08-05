f4d9ccabe1317b18dafd0c50ca3a3283da85b1ce
Bhupendra Bisht <bhupendra.bisht@oropocket.com>
2021-08-05 17:49:04 +0530

    rinkeby setup for vesting schedules

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
-export const UFARMBeneficiaryBookAddress: string = "0x349d55f12fb166a926214ca0195a07a16fa4ccb1";
+export const UFARMBeneficiaryBookAddress: string = "0x0ba3900a0E6441A1bCF7906C98Bf41ec00117f26";
 export const beneficiaryServerEndpoint = "https://transit.unifarm.co/beneficiaries";
 
-export const ADMIN_WALLET:string = "0xea771396CF2041B15058dd14688fb44d6b68E022"
+export const ADMIN_WALLET: string = "0x828954676f2634D404251f05e4F619FF83f7EceB";
 // 15 seconds per block 4*12.5 = 12.5 minuates
-export const DEFAULT_BLOCK:number=50
+export const DEFAULT_BLOCK: number = 50;
