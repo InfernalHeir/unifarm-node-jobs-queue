1ddeee758600745bef698468c0523c6aa7aac13b
Bhupendra Bisht <bhupendra.bisht@oropocket.com>
2021-08-05 17:53:13 +0530

    beneficiary server changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const UFARMBeneficiaryBookAddress: string = "0x0ba3900a0E6441A1bCF7906C98Bf41ec00117f26";
-export const beneficiaryServerEndpoint = "https://transit.unifarm.co/beneficiaries";
+export const beneficiaryServerEndpoint = "https://dev.api.unifarm.co/beneficiaries";
 
 export const ADMIN_WALLET: string = "0x828954676f2634D404251f05e4F619FF83f7EceB";
 // 15 seconds per block 4*12.5 = 12.5 minuates
