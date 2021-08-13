d449c44e85643f4bb63f5da2eb22bd5ec9fa34e5
Bhupendra Bisht <bhupendrabisht61049@gmail.com>
2021-08-13 14:42:12 +0530

    eth-mainnet push for node-queue

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,11 @@
-export const UFARMBeneficiaryBookAddress: string = "0x0ba3900a0E6441A1bCF7906C98Bf41ec00117f26";
-export const beneficiaryServerEndpoint = "https://dev.api.unifarm.co/beneficiaries";
+export const UFARMBeneficiaryBookAddress: string = "0xd5F119145bdF66998F3c33ae0fE529aD546c67F5";
+export const beneficiaryServerEndpoint = "https://transit.unifarm.co/beneficiaries";
 
 export const ADMIN_WALLET: string = "0x828954676f2634D404251f05e4F619FF83f7EceB";
 // 15 seconds per block 4*12.5 = 12.5 minuates
 export const DEFAULT_BLOCK:number=50;
 
-export const SUPPORTED_CHAINID=4;
+export const SUPPORTED_CHAINID=1;
 
 export const ZERO_ADDRESS: string = "0x0000000000000000000000000000000000000000";
 
